d5f861570ad35f461c6bd736c119b3d1eb41d4dc
Santiago Cepeda <santyagor@outlook.com>
2021-08-16 17:13:24 -0500

    Add entity

---
+++ TodoSanty.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace TodoSanty.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
